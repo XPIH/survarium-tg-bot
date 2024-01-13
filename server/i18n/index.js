ca5b5f341c6a2335af6ffb4dcb917feb475358bf
XPIH <46504127+XPIH@users.noreply.github.com>
2024-01-13 14:24:59 +0200

    Update index.js

--- server/i18n/index.js
+++ server/i18n/index.js
@@ -1,9 +1,9 @@
 const DICT = {
-    ru: require('./ru')
+    ua: require('./ua')
 };
 
 const LANGS = Object.keys(DICT);
-const DEFAULT_LANG = 'ru';
+const DEFAULT_LANG = 'ua';
 
 module.exports = (key, opts) => {
     let lang = opts && opts.lang && LANGS.includes(opts.lang) && opts.lang || DEFAULT_LANG;
